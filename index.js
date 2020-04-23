c74475c0c89c1ddc170972ffe3bae99ba1066bf2
akhil-rana <akhilrana0001@gmail.com>
2020-04-23 07:23:22 +0530

    commandLine_YTDL_DONE

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 const ytdl = require("ytdl-core");
 const express = require("express");
-const request = require("request");
+const ffmpeg = require("fluent-ffmpeg");
 const app = express();
-var readline = require("readline-sync");
+const readline = require("readline-sync");
 const PORT = 8080;
-app.use(cors());
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // var server = app.listen(process.env.PORT || 8080, () =>
 //   console.log("App listening on port " + PORT)
 // );
+
 ytdl.getInfo("https://www.youtube.com/watch?v=E3LeZNlI0Xg").then((response) => {
   showDetails(response);
 });
@@ -52,14 +53,41 @@ function showDetails(info) {
 }
 
 function downloadFile(name, url, formats) {
-  let input = readline.question("Enter the video index: ");
-  let file = fs.createWriteStream(name + "." + formats[Number(input - 1)]);
-  https.get(url[Number(input - 1)], function (response) {
-    response.pipe(file);
+  let vinput = readline.question("Enter the video index: ");
+  let vfile = fs.createWriteStream("video." + formats[Number(vinput - 1)]);
+  let ainput = readline.question("Enter the audio index: ");
+  let afile = fs.createWriteStream("audio." + formats[Number(ainput - 1)]);
+
+  let vdown = new Promise(function (resolve, reject) {
+    https.get(url[Number(vinput - 1)], function (response) {
+      let vstream = response.pipe(vfile);
+      vstream.on("finish", function () {
+        resolve("adone");
+      });
+    });
   });
-  let input1 = readline.question("Enter the audio index: ");
-  let file1 = fs.createWriteStream("Audio - "+name + "." + formats[Number(input1 - 1)]);
-  https.get(url[Number(input1 - 1)], function (response) {
-    response.pipe(file1);
+  let adown = new Promise(function (resolve, reject) {
+    https.get(url[Number(ainput - 1)], function (response) {
+      let astream = response.pipe(afile);
+      astream.on("finish", function () {
+        resolve("adone");
+      });
+    });
+  });
+
+  Promise.all([vdown, adown]).then(function () {
+    ffmpeg("./video." + formats[Number(vinput - 1)])
+      .input("./audio." + formats[Number(ainput - 1)])
+      .audioCodec("copy")
+      .videoCodec("copy")
+      .on("error", function (err) {
+        console.log("An error occurred: " + err.message);
+      })
+      .on("end", function () {
+        console.log("Processing finished !");
+        fs.unlink("./video." + formats[Number(vinput - 1)]);
+        fs.unlink("./audio." + formats[Number(ainput - 1)]);
+      })
+      .save("./output.mkv");
   });
 }
